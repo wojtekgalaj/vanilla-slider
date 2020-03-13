abbffb17b680c7d3e0218a4642f4d12510ac2a77
Wojtek G <wojtek.galaj@gmail.com>
2020-03-13 09:56:01 +0100

    Initial commit, copies from wbn, not original

---
+++ src/youtube-player.js
@@ -0,0 +1,33 @@
+(function(window) {
+  console.log("script running");
+
+  const tag = document.createElement("script");
+  tag.src = "https://www.youtube.com/iframe_api";
+  var firstScriptTag = document.getElementsByTagName("script")[0];
+  firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+  let player;
+
+  function onYouTubeIframeAPIReady() {
+    console.log("youtube iframe ready");
+
+    player = new YT.Player("youtube-player-1", {
+      height: "390",
+      width: "640",
+      videoId: "M7lc1UVf-VE",
+      events: {
+        onReady: onPlayerReady,
+        onStateChange: onPlayerStateChange
+      }
+    });
+  }
+  function onPlayerReady(event) {
+    event.target.playVideo();
+  }
+  function onPlayerStateChange(event) {
+    console.log("on player state change ", event);
+  }
+
+  window.onYouTubeIframeAPIReady = onYouTubeIframeAPIReady;
+  window.onPlayerReady = onPlayerReady;
+  window.onPlayerStateChange = onPlayerStateChange;
+})(window);
