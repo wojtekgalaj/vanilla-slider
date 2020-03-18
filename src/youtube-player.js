3c6464f015baf59f06282490e69ded94aa93247f
Wojtek G <wojtek.galaj@gmail.com>
2020-03-18 12:47:55 +0100

    Correctly placed video

--- src/youtube-player.js
+++ src/youtube-player.js
@@ -5,29 +5,58 @@
   tag.src = "https://www.youtube.com/iframe_api";
   var firstScriptTag = document.getElementsByTagName("script")[0];
   firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-  let player;
+  let players = [];
+  let videoIds = [];
 
   function onYouTubeIframeAPIReady() {
     console.log("youtube iframe ready");
 
-    player = new YT.Player("youtube-player-1", {
-      height: "390",
-      width: "640",
-      videoId: "M7lc1UVf-VE",
-      events: {
-        onReady: onPlayerReady,
-        onStateChange: onPlayerStateChange
-      }
-    });
+    const playerContainers = document.getElementsByClassName("youtube-video");
+
+    console.log("player containers ", playerContainers);
+
+    if (playerContainers.length !== videoIds.length) {
+      conosole.error(
+        "The number of video containers and the ids you have passed in don't match"
+      );
+      return;
+    }
+
+    console.log("length ", playerContainers.length);
+    for (let i = 0; i < playerContainers.length; i++) {
+      const container = playerContainers[i];
+      const containerParent = container.parentNode;
+      const clientRect = containerParent.getBoundingClientRect();
+
+      const player = new YT.Player(container, {
+        videoId: videoIds[i],
+        heigth: clientRect.height + "",
+        width: clientRect.width + "",
+        events: {
+          onReady: onPlayerReady,
+          onStateChange: onPlayerStateChange
+        }
+      });
+      players.push(player);
+    }
   }
   function onPlayerReady(event) {
     /* event.target.playVideo(); */
+    console.log("on player reasy ", event);
   }
   function onPlayerStateChange(event) {
     console.log("on player state change ", event);
   }
 
+  function initPlayers(ids) {
+    videoIds = ids;
+  }
+
+  win.onresize = function(ev) {
+    const player = players[0];
+  };
   win.onYouTubeIframeAPIReady = onYouTubeIframeAPIReady;
   win.onPlayerReady = onPlayerReady;
   win.onPlayerStateChange = onPlayerStateChange;
+  win.initPlayers = initPlayers;
 })(window);
